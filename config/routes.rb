466e7dfb2e6103da2aea71619d4107fda7aba68c
chabikun <yu_ichiro_m@yahoo.co.jp>
2019-03-22 00:17:36 +0900

    Revert "修正"
    This reverts commit a3aa84fc0a7debc07495d30ee1593e5f6340adef.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :signup, only: [:new]
+  get 'signup/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
