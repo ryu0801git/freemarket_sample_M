5b62ddc2b895afe3fbd58c4797db32e7a9fd4a43
haccaman <haccaman@gmail.com>
2019-03-23 15:55:05 +0900

    Release conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root 'items#index'
-=======
   resources :signup, only: [:new]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> master
 end
